17725f1da0abca58f425c3d0232ce2f31e7b6438
ideal123 <iseeucome@outlook.com>
2018-11-09 00:10:23 +0800

    提取图片模糊组件

---
+++ src/components/base/FuzzBg.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="fuzz-bg">
+    <div class="fuzz-bg-img" :style="{ 'background-image': `url('${src}')` }"></div>
+    <div class="fuzz-bg-cover"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      required: true,
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.fuzz-bg {
+  width: 100%;
+  height: 100%;
+  &-img {
+    width: 100%;
+    height: 100%;
+    background: no-repeat 50% 50%;
+    background-size: cover;
+    transform: scale(1.5, 1.5);
+    filter: blur(40px);
+  }
+  &-cover {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    opacity: 0.3;
+    background: black;
+  }
+}
+</style>
--- src/views/FM.vue
+++ src/views/FM.vue
@@ -1,3 +1,27 @@
 <template>
-  <div class="fm">fm</div>
+  <div class="fm">
+    <FuzzBg :src="'http://p1.music.126.net/SJaaWVbYS-y7p6DjNbm6ZA==/109951163653441372.jpg'" />
+  </div>
 </template>
+
+<script>
+import FuzzBg from "@/components/base/FuzzBg";
+
+export default {
+  components: {
+    FuzzBg
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.fm {
+  z-index: 1012;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: white;
+}
+</style>
